0d0959faa96e6573761459af1c5bd7f3976042ea
Jorge Atempa <atempaitm@gmail.com>
2016-10-15 23:40:01 -0700

    Agregar método de interpolación cuadrática

--- interopolation.hs
+++ interopolation.hs
@@ -1,3 +1,31 @@
-lineal xs a = (+) (snd (xs !! 0)) (pendiente xs * (a - fst (xs !! 0))) 
+lineal :: Fractional b => [(b, b)] -> b -> b
+lineal xs x = (+) (_x0 xs) (pendiente xs * (x - (_x1 xs)))
 
-pendiente ys = (/) (snd (ys !! 1) - snd (ys !! 0)) (fst (ys !! 1) - fst (ys !! 0)) 
+cuadratica :: Fractional b => [(b, b)] -> b -> b
+cuadratica xs x = (_b0 xs) + ((_b1 xs x) * (x - (_x1 xs))) + (_b2 xs * (x - (_x1 xs)) * (x - (_x2 xs)))
+
+pendiente :: Fractional b => [(b, b)] -> b
+pendiente xs = (/) (snd (xs !! 1) - snd (xs !! 0)) (fst (xs !! 1) - fst (xs !! 0))
+
+_x0 :: [(a, b)] -> b
+_x0 xs = snd (xs !! 0)
+
+_x1 :: [(a, b)] -> a
+_x1 xs = fst (xs !! 0)
+
+_x2 :: [(a, b)] -> a
+_x2 xs = fst (xs !! 1)
+
+_b0 :: [(a, b)] -> b
+_b0 xs = _x0 xs
+
+_b1 :: Fractional b => [(b, b)] -> b -> b
+_b1 xs x = pendiente (init xs)
+
+_b2 :: Fractional b => [(b, b)] -> b
+_b2 xs = ((/) (pendiente (tail xs) - pendiente (init xs)) (fst (xs !! 2) - fst (xs !! 0)))
+
+interpolar :: (Fractional a, Show a) => [(a, a)] -> a -> [Char]
+interpolar xs x | length xs == 2 = "f(x) = " ++ show (lineal xs x)
+                | length xs == 3 = "f(x) = " ++ show (cuadratica xs x)
+                | otherwise	     = "Error: parametros invalidos para su evaluacion."
