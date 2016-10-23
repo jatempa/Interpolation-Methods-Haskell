876b70e856c770c38b92a3d62ac4932f101a72d4
Jorge Atempa <atempaitm@gmail.com>
2016-10-22 19:25:09 -0700

    Definir variables

--- interopolation.hs
+++ interopolation.hs
@@ -1,24 +1,23 @@
 lineal :: Fractional b => [(b, b)] -> b -> b
-lineal xs x = (+) (_x0 xs) (pendiente xs * (x - (_x1 xs)))
+lineal xs x = 
+	let x0 = snd (xs !! 0)
+	    x1 = fst (xs !! 0)
+	in (+) x0 (pendiente xs * (x - x1))
 
 cuadratica :: Fractional b => [(b, b)] -> b -> b
 cuadratica xs x = 
-	let _b0 = _x0 xs
-	    _b1 = pendiente (init xs)
-	    _b2 = ((/) (pendiente (tail xs) - pendiente (init xs)) (fst (xs !! 2) - fst (xs !! 0)))
-	in _b0 + (_b1 * (x - (_x1 xs))) + (_b2 * (x - (_x1 xs)) * (x - (_x2 xs)))
+	let b0 = snd (xs !! 0)
+	    b1 = pendiente (init xs)
+	    b2 = ((/) (pendiente (tail xs) - pendiente (init xs)) (fst (xs !! 2) - fst (xs !! 0)))
+	in b0 + (b1 * (x - (fst (xs !! 0)))) + (b2 * (x - (fst (xs !! 0))) * (x - (fst (xs !! 1))))
 
 pendiente :: Fractional b => [(b, b)] -> b
-pendiente xs = (/) (snd (xs !! 1) - snd (xs !! 0)) (fst (xs !! 1) - fst (xs !! 0))
-
-_x0 :: [(a, b)] -> b
-_x0 xs = snd (xs !! 0)
-
-_x1 :: [(a, b)] -> a
-_x1 xs = fst (xs !! 0)
-
-_x2 :: [(a, b)] -> a
-_x2 xs = fst (xs !! 1)
+pendiente xs = 
+	let y0 = snd (xs !! 0)
+	    y1 = snd (xs !! 1)
+	    x0 = fst (xs !! 0)
+	    x1 = fst (xs !! 1)
+	in (/) (y1 - y0) (x1 - x0)
 
 interpolar :: (Fractional a, Show a) => [(a, a)] -> a -> [Char]
 interpolar xs x | length xs == 2 = "f(x) = " ++ show (lineal xs x)
