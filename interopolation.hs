39108039d219b424a5d4c7e1cbc73a7a090f1971
Jorge Atempa <atempaitm@gmail.com>
2017-01-08 19:07:25 -0800

    Renombrar variables

--- interopolation.hs
+++ interopolation.hs
@@ -1,24 +1,24 @@
 -- Calcular pendiente
 pendiente :: Fractional b => [(b, b)] -> b
-pendiente xs = 
-    let y0 = snd (xs !! 0)
-        y1 = snd (xs !! 1)
-        x0 = fst (xs !! 0)
-        x1 = fst (xs !! 1)
+pendiente lst = 
+    let y0 = snd (lst !! 0)
+        y1 = snd (lst !! 1)
+        x0 = fst (lst !! 0)
+        x1 = fst (lst !! 1)
     in  (/) (y1 - y0) (x1 - x0)
 -- Interpolacion Lineal
 lineal :: Fractional b => [(b, b)] -> b -> b
-lineal xs x =
-    let x0 = snd (xs !! 0)
-        x1 = fst (xs !! 0)
-    in  (+) x0 (pendiente xs * (x - x1))
+lineal lst x =
+    let x0 = snd (lst !! 0)
+        x1 = fst (lst !! 0)
+    in  (+) x0 (pendiente lst * (x - x1))
 -- Interpolacion Cuadratica
 cuadratica :: Fractional b => [(b, b)] -> b -> b
-cuadratica xs x =
-    let b0 = snd (xs !! 0)
-        b1 = pendiente (init xs)
-        b2 = ((/) (pendiente (tail xs) - pendiente (init xs)) (fst (xs !! 2) - fst (xs !! 0)))
-    in  b0 + (b1 * (x - (fst (xs !! 0)))) + (b2 * (x - (fst (xs !! 0))) * (x - (fst (xs !! 1))))
+cuadratica lst x =
+    let b0 = snd (lst !! 0)
+        b1 = pendiente (init lst)
+        b2 = ((/) (pendiente (tail lst) - pendiente (init lst)) (fst (lst !! 2) - fst (lst !! 0)))
+    in  b0 + (b1 * (x - (fst (lst !! 0)))) + (b2 * (x - (fst (lst !! 0))) * (x - (fst (lst !! 1))))
 -- Metodo de Lagrange
 lagrange :: Fractional b => [(b, b)] -> b -> b
 lagrange lst x =
@@ -30,8 +30,8 @@ lagrange lst x =
     in  sum q
 -- Metodo principal
 interpolar :: (Fractional a, Show a) => [(a, a)] -> a -> [Char]
-interpolar xs x 
-    | length xs == 2 = "f(" ++ show x ++ ") = " ++ show (lineal xs x)
-    | length xs == 3 = "f(" ++ show x ++ ") = " ++ show (cuadratica xs x)
-    | length xs >= 4 = "f(" ++ show x ++ ") = " ++ show (lagrange xs x)
+interpolar lst x 
+    | length lst == 2 = "f(" ++ show x ++ ") = " ++ show (lineal lst x)
+    | length lst == 3 = "f(" ++ show x ++ ") = " ++ show (cuadratica lst x)
+    | length lst >= 4 = "f(" ++ show x ++ ") = " ++ show (lagrange lst x)
     | otherwise      = "Error: parametros invalidos para su evaluacion."
