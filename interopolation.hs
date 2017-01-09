a20a361e3478d60a981ce9b52b3bdcdc1332fdff
Jorge Atempa <atempaitm@gmail.com>
2017-01-08 16:38:52 -0800

    Modificar firma

--- interopolation.hs
+++ interopolation.hs
@@ -14,14 +14,14 @@ lineal xs x =
 	in  (+) x0 (pendiente xs * (x - x1))
 -- Interpolacion Cuadratica
 cuadratica :: Fractional b => [(b, b)] -> b -> b
+cuadratica [] _ = 0
 cuadratica xs x =
 	let b0 = snd (xs !! 0)
 	    b1 = pendiente (init xs)
 	    b2 = ((/) (pendiente (tail xs) - pendiente (init xs)) (fst (xs !! 2) - fst (xs !! 0)))
 	in  b0 + (b1 * (x - (fst (xs !! 0)))) + (b2 * (x - (fst (xs !! 0))) * (x - (fst (xs !! 1))))
 -- Metodo de Lagrange
-lagrange :: (Eq t, Fractional t) => [(t, t)] -> t -> t
-lagrange [] _ = 0
+lagrange :: Fractional b => [(b, b)] -> b -> b
 lagrange lst x =
 	let n   = length lst - 1
 	    xs  = map fst lst
@@ -30,7 +30,7 @@ lagrange lst x =
 	    q   = [(ys !! i) * (p i)| i <- [0..n]]
 	in  sum q
 -- Metodo principal
-interpolar :: (Eq a, Fractional a, Show a) => [(a, a)] -> a -> [Char]
+interpolar :: (Fractional a, Show a) => [(a, a)] -> a -> [Char]
 interpolar xs x | length xs == 2 = "f(" ++ show x ++ ") = " ++ show (lineal xs x)
                 | length xs == 3 = "f(" ++ show x ++ ") = " ++ show (cuadratica xs x)
                 | length xs >= 4 = "f(" ++ show x ++ ") = " ++ show (lagrange xs x)
