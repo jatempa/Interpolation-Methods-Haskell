bc570dff2bc8c1bd60a0290d9bcdde1050f33f0b
Jorge Atempa <atempaitm@gmail.com>
2017-03-20 19:04:02 -0700

    Refactorizar la impresión de resultado

--- main.hs
+++ main.hs
@@ -1,16 +1,17 @@
 import Interpolacion
 
 -- Metodo interpolar
-interpolar :: (Fractional a, Show a) => [(a, a)] -> a -> [Char]
+interpolar :: Fractional b => [(b, b)] -> b -> b
 interpolar lst x
-    | length lst == 2 = "f(" ++ show x ++ ") = " ++ show (lineal lst x)
-    | length lst == 3 = "f(" ++ show x ++ ") = " ++ show (cuadratica lst x)
-    | length lst >= 4 = "f(" ++ show x ++ ") = " ++ show (lagrange lst x)
-    | otherwise      = "Error: parametros invalidos para su evaluacion."
+    | length lst == 2 = lineal lst x
+    | length lst == 3 = cuadratica lst x
+    | length lst >= 4 = lagrange lst x
 
+--Ejemplo 
+--[(0,0), (10,250), (15,350), (22,655), (25,890), (30,910)]
 main = do
     putStrLn "Introduce una lista de pares ordenados [(x1,y1),(x2,y3),...,(xN,yN)]"
     l <- getLine
     putStrLn "Introduce una incognita x"
     a <- getLine
-    putStrLn $ "Resultado de " ++ interpolar (read l) (read a)
+    putStrLn $ "Resultado de f(" ++ show a ++ ") = " ++ show (interpolar (read l) (read a))
